f97b378e4c4bd993e05263ee666b98135295f59c
Amit Kumar Santra <mailme.amitsantra@gmail.com>
2020-09-22 23:54:53 +0530

    Appium Basic test

--- AppiumTest/BaseTest.cs
+++ AppiumTest/BaseTest.cs
@@ -1,14 +1,33 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
 
 namespace AppiumTest
 {
     [TestClass]
     public class BaseTest
     {
-        [TestMethod]
-        public void TestMethod1()
+        private const string CapabilityValue = "donatelo";
+        private const string Capability = "deviceName";
+
+        //Crearting Instance for Appium 
+        AppiumDriver<IWebElement> driver;
+
+        private void Init()
         {
+            AppiumOptions dc = new AppiumOptions();
+            dc.AddAdditionalCapability("", "");
+            driver = new AndroidDriver<IWebElement>(new Uri("", dc));
+
+        }
+
+        [TestMethod]
+        public void AppiumFirstTest()
+        {
+
         }
     }
 }
